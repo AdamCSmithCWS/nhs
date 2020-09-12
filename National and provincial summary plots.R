2122da49cfe882cb1f532cb4b8674efdcd4a96cb
Adam C Smith <adam.smith2@canada.ca>
2020-09-11 21:52:22 -0400

    Updating publication figures

--- National and provincial summary plots.R
+++ National and provincial summary plots.R
@@ -62,6 +62,12 @@ names(pubEsts_age_sex_all) <- c("sp","species","prov","zone","year","age_ratio")
 pubEsts_age_sex_all[which(is.na(pubEsts_age_sex_all$prov)),"prov"] <- "Canada"
 
 
+pubEsts_age_sex_all <- pubEsts_age_sex_all[which(pubEsts_age_sex_all$year > 1975),]
+pubEsts_species_all <- pubEsts_species_all[which(pubEsts_species_all$year > 1975),]
+pubEsts_simple_all <- pubEsts_simple_all[which(pubEsts_simple_all$year > 1975),]
+
+
+
 species_web_names = unique(pubEsts_species_all[,c("sp","species")])
 
 var_names_sim <- unique(pubEsts_simple_all[,c("var","name")])
